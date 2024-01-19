8933c605ab3fdff5724b4d883fbf54ae3ddc39a1
j-mahapatra <jagannathm.official@gmail.com>
2024-01-19 15:51:28 +0530

    feat: create Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className='bg-secondary'>
       <Navbar />
-      <section>Hero Section</section>
-      <section>Booking Section</section>
-      <section>Testimonials</section>
+      <section id='hero' className='h-screen'>
+        <Hero />
+      </section>
+      <section id='booking' className='h-screen'>
+        Booking Section
+      </section>
+      <section id='testimonials' className='h-screen'>
+        Testimonials
+      </section>
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,24 @@
+export default function Hero() {
+  return (
+    <div className='flex flex-col sm:flex-row p-12 justify-center sm:justify-start'>
+      <div className='flexCenter flex-col sm:items-start p-5 w-full sm:w-2/3'>
+        <p className='text-white text-6xl sm:text-7xl text-center sm:text-left'>
+          live life,
+        </p>
+        <p className='text-6xl sm:text-7xl textGradient text-center sm:text-left'>
+          pain free
+        </p>
+        <a
+          href='#booking'
+          className='flex w-fit box-border p-3 my-8 text-white hover:bg-primary hover:text-secondary transition-all border border-primary rounded-full'
+        >
+          Book an appointment
+        </a>
+      </div>
+      <div className='relative flexCenter w-fit aspect-w-16 aspect-h-9'>
+        <img src='doctor-image.png' alt='Hero Image' className='w-full z-10' />
+        <div className='absolute h-full w-full backgroundGradient' />
+      </div>
+    </div>
+  );
+}
